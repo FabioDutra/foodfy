6f86b07107c0680d1a94225d3c2b4ca312ada47f
Fábio Dutra <fabio.dutrafig@gmail.com>
2020-11-08 17:48:14 -0400

    inclusão de modal

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,19 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+
+for (let card of cards) {
+  card.addEventListener('click', function () {
+    const imageId = card.getAttribute('id');
+    modalOverlay.classList.add('active');
+    modalOverlay.querySelector('img').src = `../public/assets/images/${imageId}`;
+  });
+}
+
+
+
+modalOverlay
+  .querySelector('.close-modal')
+  .addEventListener('click', function () {
+    modalOverlay.classList.remove('active');
+  });
