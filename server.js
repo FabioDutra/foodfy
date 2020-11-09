cb11171b5dab87e16e8dab675fd5086b716ee2a4
Fábio Dutra <fabio.dutrafig@gmail.com>
2020-11-09 11:32:30 -0400

    refatoração com nunjucks

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('views', {
+  express: server,
+});
+
+
+//Routes
+server.get('/', function (req, res) {
+  return res.render('home');
+});
+
+
+server.get('/about', function(req, res) {
+    return res.render('about')
+})
+
+server.get('/recipes', function(req, res) {
+    return res.render('recipes')
+})
+
+// end Routes
+
+server.listen(5000, function () {
+  console.log('Server is running');
+});
